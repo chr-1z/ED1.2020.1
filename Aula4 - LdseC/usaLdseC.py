6415977ca62c357f14732918f5ab45822fa23cac
chr-1z <51412784+chr-1z@users.noreply.github.com>
2020-03-03 21:26:28 -0300

    Add files via upload

---
+++ Aula4 - LdseC/usaLdseC.py
@@ -0,0 +1,23 @@
+import LdseC
+
+l = LdseC.LdseC()
+
+l.inserirInicio('B')
+
+l.inserirInicio('A')
+
+#l.inserirFim('C')
+
+#l.removerInicio()
+
+#l.removerInicio()
+
+#l.removerFim()
+
+#l.removerFim()
+
+l.show()
+
+
+
+
